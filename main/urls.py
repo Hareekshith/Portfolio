07b845c86a2153f86e5e198e0ee08d387e4944cc
hareekshith <hareekshith@gmail.com>
2025-09-03 23:21:30 +0530

    Converted Django project to flask

--- main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-        path('',views.home,name='home'),
-        path('exp/',views.exp,name='exp'),
-        path('soc/',views.soc,name='soc'),
-        path('tl/',views.tl,name='tl')
-]
